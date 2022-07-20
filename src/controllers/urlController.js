47e9bcb77a953dcc16cee67a7e93074b645dedff
DonIndra95 <royindrashish95@gmail.com>
2022-07-20 19:09:50 +0530

    Bug fixing

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -38,18 +38,21 @@ const urlShorten = async (req, res) => {
         .status(400)
         .send({ status: false, message: "Enter a valid input in body" });
 
-    let { longUrl } = req.body;
-
     //validating longUrl
-    if (!longUrl)
+    if (!req.body.longUrl)
       return res
         .status(400)
         .send({ status: false, message: "Please enter longUrl" });
 
-    if (!isValidRequest(longUrl))
+    if (!isValidRequest(req.body.longUrl))
       return res
         .status(400)
-        .send({ status: false, message: "Enter a valid longUrl" });
+        .send({ status: false, message: "Enter a valid longUrl" });    
+
+    if(!/^(http|https):/.test(req.body.longUrl))req.body.longUrl="https://"+req.body.longUrl;
+
+    let { longUrl } = req.body;
+    longUrl=longUrl.trim()
 
     if (
       !/^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})?$/.test(
@@ -60,10 +63,17 @@ const urlShorten = async (req, res) => {
         .status(400)
         .send({ status: false, message: `'${longUrl}' is not a valid URL` });
 
+    //Checking if long URL is already present in our cache
+    let cachedData= await GET_ASYNC(`${longUrl}`)
+    if(cachedData)
+     return res.status(200).send({ status: true, data: JSON.parse(cachedData) });
+
     //Checking if long URL is already present in our DB
     let checkLongUrl = await urlModel.findOne({ longUrl: longUrl }).select({ _id: 0, longUrl: 1, urlCode: 1, shortUrl: 1 });
-    if (checkLongUrl)
-      return res.status(200).send({ status: true, data: checkLongUrl });
+    if (checkLongUrl){
+      await SET_ASYNC(`${longUrl}`,600, JSON.stringify(checkLongUrl));
+      return res.status(200).send({ status: true, data: checkLongUrl });      
+    }
 
     //creating urlCode
     let short = shortid.generate().toLowerCase();
@@ -79,10 +89,12 @@ const urlShorten = async (req, res) => {
     let savedData = await urlModel.create(req.body);
 
     let data = {
-      urlCode: savedData.urlCode,
       longUrl: savedData.longUrl,
       shortUrl: savedData.shortUrl,
+      urlCode: savedData.urlCode
     };
+
+    await SET_ASYNC(`${longUrl}`,600, JSON.stringify(data));
 
     res.status(201).send({ status: true, data: data });
   } catch (err) {
@@ -108,28 +120,29 @@ const getUrl = async (req, res) => {
         status: false,
         message: "Please enter a valid urlCode",
       });
+
+      urlCode=urlCode.toLowerCase().trim()
     
     //Checking if data is present in cache,and sending response if present
     let cachedData = await GET_ASYNC(`${urlCode}`);
     if (cachedData) {
-      return res.status(302).send(cachedData);
+      return res.status(302).redirect(cachedData);
     }
 
     //If not present in cache fetching data from DB and storing it in cache
-    let checkUrl = await urlModel.findOne({
-      urlCode: urlCode.toLowerCase().trim(),
-    });
+    let checkUrl = await urlModel.findOne({urlCode: urlCode});
     
     if (!checkUrl)
       return res.status(404).send({ status: false, message: "URL not found" });
     
-    await SET_ASYNC(`${urlCode}`,60, `Found. Redirecting to ${checkUrl.longUrl}`);
-    console.log("db call")
+    await SET_ASYNC(`${urlCode}`,60, checkUrl.longUrl);
+   // console.log("db call")
     res.status(302).redirect(checkUrl.longUrl);
 
   } catch (err) {
 
     res.status(500).send({ sattus: false, message: err.message });
+
   }
 };
 
--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const urlSchema = new mongoose.Schema(
   {
+    longUrl: { type: String, required: true, trim: true },
+    shortUrl: { type: String, required: true, unique: true },
     urlCode: {
       type: String,
       required: true,
@@ -9,8 +11,6 @@ const urlSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
-    longUrl: { type: String, required: true, trim: true },
-    shortUrl: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
